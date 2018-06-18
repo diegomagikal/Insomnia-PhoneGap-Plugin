d01070042471132065e1a2ce53941d1d79f4ad36
Diego Luiz <diego@magikal.com>
2018-06-17 22:06:03 -0300

    add _sleepingAlowed parameter to function Insonmia

--- www/Insomnia.js
+++ www/Insomnia.js
@@ -2,14 +2,20 @@
 
 var exec = require('cordova/exec');
 
+function Insomnia(){
+	this._sleepingAlowed = true;
+}
+
 Insomnia.prototype = {
 
 		keepAwake: function (successCallback, errorCallback) {
 		  exec(successCallback, errorCallback, "Insomnia", "keepAwake", []);
+		  this._sleepingAlowed = false;
 		},
 
 		allowSleepAgain: function (successCallback, errorCallback) {
 		  exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
+		  this._sleepingAlowed = true;
 		},
 
 
