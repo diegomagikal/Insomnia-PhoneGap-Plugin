a3e92cd47296e7f27abfa8bc710e1e49696241c1
Diego Luiz <diego@magikal.com>
2018-06-17 21:56:30 -0300

    class refactor

--- www/Insomnia.js
+++ www/Insomnia.js
@@ -1,21 +1,29 @@
-function Insomnia() {
+'use strict';
+
+var exec = require('cordova/exec');
+
+Insomnia.prototype = {
+
+		keepAwake: function (successCallback, errorCallback) {
+		  exec(successCallback, errorCallback, "Insomnia", "keepAwake", []);
+		},
+
+		allowSleepAgain: function (successCallback, errorCallback) {
+		  exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
+		},
+
+
 }
 
-Insomnia.prototype.keepAwake = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Insomnia", "keepAwake", []);
-};
 
-Insomnia.prototype.allowSleepAgain = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
-};
+Insomnia.install = function(){
+	if (!window.plugins) {
+	  window.plugins = {};
+	}
 
-Insomnia.install = function () {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
+	window.plugins.insomnia = new Insomnia();
+	return window.plugins.insomnia;	
+}
 
-  window.plugins.insomnia = new Insomnia();
-  return window.plugins.insomnia;
-};
 
 cordova.addConstructor(Insomnia.install);
